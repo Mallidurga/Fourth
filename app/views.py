eef539febdfe6fc8cef3164da847860871f4aca7
Mallidurga <mallidurga82@gmail.com>
2022-03-16 20:50:47 +0530

    fourth commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def fourth(request):
+    return HttpResponse('<Marquee><h1>Mallikarjunareddy</h1></marquee>')
